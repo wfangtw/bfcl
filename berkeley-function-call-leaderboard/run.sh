1c35a33cfe465268265e1443211ef365069c0d19
wfangtw <b40815@gmail.com>
2025-01-05 21:59:49 -0500

    fix score_dir bufg

--- berkeley-function-call-leaderboard/run.sh
+++ berkeley-function-call-leaderboard/run.sh
@@ -2,8 +2,9 @@
 #
 source ${HOME}/miniconda3/etc/profile.d/conda.sh
 conda activate bfcl
-bfcl generate --model meta-llama/Llama-3-8B-Instruct-FC --test-category exec_custom01 --num-threads 1 --temperature 0.1
+TEST_SET=exec_custom01
+bfcl generate --model meta-llama/Llama-3-8B-Instruct-FC --test-category ${TEST_SET} --num-threads 1 --temperature 0.1
 # --result-dir --allow-overwrite
-bfcl evaluate --model meta-llama/Llama-3-8B-Instruct-FC --test-category exec_custom01
+bfcl evaluate --model meta-llama/Llama-3-8B-Instruct-FC --test-category ${TEST_SET}
 # --result-dir --score-dir
 conda deactivate
